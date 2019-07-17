27fb08eb8886e139a2352b2bad39c627ae250cea
Slav666 <slawomirdyk@gmail.com>
2019-07-17 17:36:45 +0100

    New project setup

---
+++ src/components/movies.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'; 
+
+ class Movies extends Component {
+     state = {  }
+     render() { 
+         return ( 
+             <h1>List of Movies</h1>
+          );
+     }
+ }
+  
+ export default Movies;
