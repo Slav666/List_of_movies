7a91ce83bcf248c11ac0c5fc2fc659384d7554c1
Slav666 <slawomirdyk@gmail.com>
2019-07-17 19:45:21 +0100

    Add onClick event and handleDelete function

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,12 @@ class Movies extends Component {
     state = {
         movies: getMovies()
     }
+
+    handleDelete = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id)
+        this.setState({movies})
+    }
+
     render() { 
         return ( 
             <React.Fragment>
@@ -27,7 +33,7 @@ class Movies extends Component {
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
                             <td>
-                                <button className="btn btn-danger"> 
+                                <button className="btn btn-danger" onClick={() => this.handleDelete(movie)}> 
                                     Delete
                                 </button>
                             </td>
