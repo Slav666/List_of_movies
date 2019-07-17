c366f36319a8f5c19be22b6373da476df0193c42
Slav666 <slawomirdyk@gmail.com>
2019-07-17 18:18:28 +0100

    The main table for the list of movies created

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'; 
 
- class Movies extends Component {
-     state = {  }
-     render() { 
-         return ( 
+class Movies extends Component {
+
+    
+    render() { 
+        return ( 
+            <React.Fragment>
              <h1>List of Movies</h1>
-          );
-     }
- }
+                <table class="table table-dark">
+                    <thead>
+                        <tr style={{color: 'blue'}}>
+                            <th scope="col">Title</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Rate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
+    }
+}
   
- export default Movies;
+export default Movies;
