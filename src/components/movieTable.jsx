acd21c19a61fc405960a81feb75cecca608d1628
Slav666 <slawomirdyk@gmail.com>
2019-07-19 12:36:29 +0100

    Extract Tablebody to sort mixed level of abstraction

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'; 
 import TableHeader from './tableHeader';
+
+import TableBody from './tableBody';
   
 
 class MovieTable extends Component {
@@ -9,34 +11,25 @@ class MovieTable extends Component {
         {path: 'genre.name', label: 'Genre'},
         {path: 'numberInStock', label: 'Stock'},
         {path: 'dailyRentalRate', label: 'Rate'},
-        {key: 'delete' }
+        {key: 'delete', content: movie => (<button className="btn btn-danger btn-sm" onClick={() => this.props.onDelete(movie)}> 
+        Delete
+        </button>)}
         
     ];
        
     render() { 
 
-        const {movies, onDelete, sortColumn, onSort} = this.props;
+        const {movies, sortColumn, onSort} = this.props;
 
     return ( 
         <table className="table table-dark">
-            <TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort}/>
-            <tbody> 
-                {movies.map(movie => (
-                    <tr key={movie._id}>
-                        <td>{movie.title}</td>
-                        <td>{movie.genre.name}</td>
-                        <td>{movie.numberInStock}</td>
-                        <td>{movie.dailyRentalRate}</td>
-                        <td>
-                            <button className="btn btn-danger btn-sm" onClick={() => onDelete(movie)}> 
-                                Delete
-                            </button>
-                      </td>
-                    </tr>
-                ))}
-     
-     
-            </tbody>
+            <TableHeader 
+            columns={this.columns} 
+            sortColumn={sortColumn} 
+            onSort={onSort}
+            />
+            <TableBody columns={this.columns} data={movies}/>
+            
         </table> 
     );
     }
