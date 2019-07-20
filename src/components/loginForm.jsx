bbd898bada73ad385f3ee5904c673c67184a2d8a
Slav666 <slawomirdyk@gmail.com>
2019-07-20 21:37:18 +0100

    Add form submission

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'; 
 
 class LoginForm extends Component {
-    
+    handlerOnSubmit = (event) =>{
+        event.preventDefault();
+        console.log("Submitted");
+    };
+
+
+
     render() { 
         return ( 
             <div>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={this.handlerOnSubmit}>
                 <div className="form-group"><label htmlFor="username">Username</label><input id="username"type="text" className="form-control"/></div>
                 <div className="form-group"><label htmlFor="password">Password</label><input id="password"type="text" className="form-control"/></div>
                 <button className="btn btn-primary">Login</button>
